39272007bd6d090e06819629652294b1c561b968
Artūras Bončkus <artis.b45@gmail.com>
2017-03-01 20:42:46 +0200

    Created a directory for Excersise 1

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.core import Extension
-
-MOD = "myextension"
-
-module = Extension(MOD, sources = ["myextensionmodule.c"])
-
-setup(name = MOD, ext_modules = [module])
