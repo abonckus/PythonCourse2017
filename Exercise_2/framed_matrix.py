07598310d12f6da6b734064af4db80ada7a22c4e
Artūras Bončkus <artis.b45@gmail.com>
2017-03-07 17:52:04 +0200

    Completed 2 more assignment points

---
+++ Exercise_2/framed_matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.zeros(100).reshape(10,10)
+a[0,:] = 1
+a[:,0] = 1
+a[:,-1] = 1
+a[-1,:] = 1
+
+print(a)
---
+++ Exercise_2/odd_array.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+res = np.arange(1,20,2)
+print(res);
